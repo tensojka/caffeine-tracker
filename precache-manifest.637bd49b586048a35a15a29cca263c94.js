801a4d97b42d9f8aa7434204bd9c35fe1e03ea13
Ondřej Sojka <ondrej.sojka@gmail.com>
2019-07-15 15:42:53 +0200

    deploy

---
+++ precache-manifest.637bd49b586048a35a15a29cca263c94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "211f2980c08deba9a657",
+    "url": "/caffeine-tracker/js/chunk-vendors.69f1ad5e.js"
+  },
+  {
+    "revision": "de001d3b463fe6ba8d32",
+    "url": "/caffeine-tracker/js/app.731166f5.js"
+  },
+  {
+    "revision": "4b62b835a6ebbef51c97c1e0264fd733",
+    "url": "/caffeine-tracker/index.html"
+  },
+  {
+    "revision": "211f2980c08deba9a657",
+    "url": "/caffeine-tracker/css/chunk-vendors.776d6e17.css"
+  },
+  {
+    "revision": "de001d3b463fe6ba8d32",
+    "url": "/caffeine-tracker/css/app.54a42825.css"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/caffeine-tracker/precache-manifest.637bd49b586048a35a15a29cca263c94.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "caffeine-tracker"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
